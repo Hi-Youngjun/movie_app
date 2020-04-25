ec812da184a87f4ea1dfcc278277807190cbba2a
Youngjun <ingjun8854@gmail.com>
2020-04-25 12:46:26 +1000

    Updates

---
+++ precache-manifest.2ad3ac1a65978c5d1051ec40e6a182a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c574d64f9ca69439b86a5bf59254b872",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "15579ae0d791245c5cbf",
+    "url": "/movie_app/static/css/main.21e17940.chunk.css"
+  },
+  {
+    "revision": "98be54eaa9ed6a5a4f95",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15579ae0d791245c5cbf",
+    "url": "/movie_app/static/js/main.7d777d85.chunk.js"
+  },
+  {
+    "revision": "4dcb005dec2f66fe5231",
+    "url": "/movie_app/static/js/runtime-main.615d8c0e.js"
+  }
+]);
